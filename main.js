9eb48d47df568cd7c8434a26ff43a91998335bb3
ortizpalanques1 <marco.ortizpalanques@gmail.com>
2023-09-13 16:29:40 +0100

    Creating a function to fade out. Creating class black_box. Adding div black_box. Function does not work

--- main.js
+++ main.js
@@ -98,6 +98,23 @@ function goPrevPage() {
     }
 
 }
-function fadeIn() {
-    if(zIndex = 2);
+function fadeOut() {
+    if(zIndex >= 10);
 }
+
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+var desaparece = document.getElementsByClassName('black_box');
+var desapareceLetra = document.getElementById('f4');
+fade(desaparece);
+fade(desapareceLetra);
